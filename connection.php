7ee855699b660ac48996b88558ce2d8a6e472336
Nidhi <71150706+Nidhi2834@users.noreply.github.com>
2020-10-17 15:57:40 -0400

    update database

--- connection.php
+++ connection.php
@@ -1,7 +1,13 @@
 <?php
-$hostname="localhost:3308";
+/*$hostname="localhost:3308";
 $user_name="root";
 $password="";
 $db="dbfood";
+$con=mysqli_connect($hostname,$user_name,$password,$db); */
+
+$hostname="ec2-100-25-100-81.compute-1.amazonaws.com:5432";
+$user_name="wdkvfptgpawsff";
+$password="629eecd6df9f3dbf20a1a223e020086b47a8a02aff42cdd0979c7e191282b8b7";
+$db="d7ioputcmbomqo";
 $con=mysqli_connect($hostname,$user_name,$password,$db);
 ?>
